ed354cd0a6255516879a094435e58e02431f0642
Parkgwiyoung <pgwi11@naver.com>
2026-06-09 15:41:26 +0900

    chore: push 테스트

--- backend/src/test/java/come/back/gotoday/GoTodayApplicationTests.java
+++ backend/src/test/java/come/back/gotoday/GoTodayApplicationTests.java
@@ -2,7 +2,7 @@ package come.back.gotoday;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+// testtest
 @SpringBootTest
 class GoTodayApplicationTests {
 
