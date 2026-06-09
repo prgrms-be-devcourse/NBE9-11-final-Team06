9476aa235a750a6fc4100035ad4c5302f2c331f7
hyhdb <kkminwoo@gmail.com>
2026-06-09 15:03:29 +0900

    chore: 프로젝트 초기 세팅

---
+++ src/main/java/come/back/gotoday/GoTodayApplication.java
@@ -0,0 +1,13 @@
+package come.back.gotoday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoTodayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoTodayApplication.class, args);
+    }
+
+}
