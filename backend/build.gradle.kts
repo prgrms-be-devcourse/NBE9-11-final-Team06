26575707ca9ac6697a6e08236860340bee5407e3
hyhdb <kkminwoo@gmail.com>
2026-06-10 16:30:05 +0900

    chore: 백엔드 환경 설정 구조 추가

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -26,6 +26,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-webmvc")
     implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("io.jsonwebtoken:jjwt-api:0.12.6")
     compileOnly("org.projectlombok:lombok")
     runtimeOnly("com.h2database:h2")
