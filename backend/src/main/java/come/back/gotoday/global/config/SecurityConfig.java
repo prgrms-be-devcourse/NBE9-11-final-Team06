78492981a11da68fb8f633e01fa4272154c514eb
taejin1998 <taejin1998@gmail.com>
2026-06-19 11:32:51 +0900

    feat: Prometheus Grafana 모니터링 환경 구성

--- backend/src/main/java/come/back/gotoday/global/config/SecurityConfig.java
+++ backend/src/main/java/come/back/gotoday/global/config/SecurityConfig.java
@@ -71,6 +71,7 @@ public class SecurityConfig {
                                 "/swagger-ui.html",
                                 "/v3/api-docs/**"
                         ).permitAll()
+                        .requestMatchers("/actuator/**").permitAll()
                         .requestMatchers("/api/admin/**").hasRole("ADMIN")
                         .anyRequest().authenticated()
                 )
