bdd97fe36409756890d4e0b0a913d7ac50d5f5a5
Parkgwiyoung <pgwi11@naver.com>
2026-06-23 14:26:43 +0900

    feat: 관광공사 API 클라이언트 추가(#233)

---
+++ backend/src/main/java/come/back/gotoday/external/tour/TourApiClient.java
@@ -0,0 +1,67 @@
+package come.back.gotoday.external.tour;
+
+import come.back.gotoday.external.tour.dto.TourApiResponse;
+import come.back.gotoday.external.tour.dto.TourApiResponse.TourPlaceItem;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClient;
+
+import java.net.URI;
+import java.util.List;
+
+@Slf4j
+@Component
+public class TourApiClient {
+
+    private static final String TOUR_PLACE_CONTENT_TYPE_ID = "12";
+
+    private final RestClient restClient;
+    private final TourApiProperties properties;
+
+    public TourApiClient(RestClient.Builder restClientBuilder, TourApiProperties properties) {
+        this.restClient = restClientBuilder.build();
+        this.properties = properties;
+    }
+
+    public List<TourPlaceItem> fetchTourPlaces(String areaCode, int pageNo, int numOfRows) {
+        URI uri = createAreaBasedListUri(areaCode, pageNo, numOfRows);
+
+        TourApiResponse response = restClient.get()
+                .uri(uri)
+                .retrieve()
+                .body(TourApiResponse.class);
+
+        return extractItems(response);
+    }
+
+    private URI createAreaBasedListUri(String areaCode, int pageNo, int numOfRows) {
+        String url = properties.getBaseUrl()
+                + "/areaBasedList2"
+                + "?serviceKey=" + properties.getApiKey()
+                + "&MobileOS=" + properties.getMobileOs()
+                + "&MobileApp=" + properties.getMobileApp()
+                + "&_type=json"
+                + "&contentTypeId=" + TOUR_PLACE_CONTENT_TYPE_ID
+                + "&areaCode=" + areaCode
+                + "&pageNo=" + pageNo
+                + "&numOfRows=" + numOfRows
+                + "&arrange=A";
+
+        return URI.create(url);
+    }
+
+    private List<TourPlaceItem> extractItems(TourApiResponse response) {
+        if (response == null
+                || response.getResponse() == null
+                || response.getResponse().getBody() == null
+                || response.getResponse().getBody().getItems() == null
+                || response.getResponse().getBody().getItems().getItem() == null) {
+            return List.of();
+        }
+
+        return response.getResponse()
+                .getBody()
+                .getItems()
+                .getItem();
+    }
+}
